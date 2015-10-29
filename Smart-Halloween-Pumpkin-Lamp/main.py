65f7bc881d918d7094dcb6eca31a69df94eafcfc
Viper <dev@viperize.it>
2015-10-29 12:29:25 +0100

    Update main.py

--- Smart-Halloween-Pumpkin-Lamp/main.py
+++ Smart-Halloween-Pumpkin-Lamp/main.py
@@ -1,18 +1,46 @@
 ################################################################################
 # Smart Halloween Pumpkin Lamp
 #
-# Created: 2015-10-10 16:39:58.483065
-#
+# Created by VIPER Team 2015 CC
+# Author: L. Rizzello
 ################################################################################
  
 import streams
-from drivers.neopixel import ledstrips as neo
 import rtttl
-from drivers.toishield import toishield
 import threading
+# import toishield module
+from drivers.toishield import toishield
+# import neopixel module
+from drivers.neopixel import ledstrips as neo
  
+streams.serial()
+ 
+# set detection enabled and pumpkin inactive by default
+toishield.microphone.detectionEnabled = True
+toishield.microphone.activePumpkin = False
+# set some variable for sound detection
+toishield.microphone.staticMin = 4096
+toishield.microphone.staticMax = -4096
+toishield.microphone.resetMinMaxCounter = 0
+ 
+# declare leds to blink! (http://doc.viperize.it/0.2.0.0007/ex_Neopixel_LED_Strips.html)
+leds = neo.ledstrip(toishield.led_pin, 16)
+leds.setall(0,0,0)
+leds.on()
+ 
+# semaphore initialized to 0 -> red: if a thread tries to acquire the semaphore
+# it blocks if the semaphore has not been turned 'green' (released)
+semaphore = threading.Semaphore(0)
+ 
+# define a RTTTL Halloween melody to be played by passing it the RTTTL string.
+# find more songs at http://ez4mobile.com/nokiatone/rtttf.htm and many other websites
+hsong = rtttl.tune('Halloween:d=4,o=5,b=180:8d6,8g,8g,8d6,8g,8g,8d6,8g,8d#6,8g,8d6,8g,8g,8d6,8g,8g,8d6,8g,8d#6,8g,8c#6,8f#,8f#,8c#6,8f#,8f#,8c#6,8f#,8d6,8f#,8c#6,8f#,8f#,8c#6,8f#,8f#,8c#6,8f#,8d6,8f#')
+ 
+# If you use Particle Photon uncomment the following line and short-circuit the Buzzer and Aux2 pins
+# toishield.buzzer_pin=D9.PWM
  
 def blink():
+    # blink while the pumpkin is active
     while toishield.microphone.activePumpkin:
         leds.setall(255,0,0)
         leds.on()
@@ -23,7 +51,7 @@ def blink():
     leds.setall(0,0,0)
     leds.on()
     semaphore.release()
- 
+
 def playHalloween():
     # plays halloween song two times, then disables pumpkin, but also waits
     # at the semaphore to synchronize with blinking thread
@@ -34,13 +62,18 @@ def playHalloween():
     sleep(1000)
     print("enabled again")
     toishield.microphone.detectionEnabled = True
- 
+
 def scare():
+    # this is called when the sound exceeds the threshold, waits one second
+    # and starts scaring!
     sleep(1000)
     thread(playHalloween)
     thread(blink)
- 
- 
+
+# define a function that takes a sensor object as parameter and checks the
+# maximum peak to peak extension of the signal in a preset window
+# look at this (http://doc.viperize.it/0.2.0.0007/ex_Microphone_Peak_Detector.html)
+# example for details on how the sound detector works
 def detectSound(obj):
     if (obj.resetMinMaxCounter == obj._observationWindowN):
         extension = obj.staticMax - obj.staticMin
@@ -58,23 +91,9 @@ def detectSound(obj):
         elif (c < obj.staticMin):
             obj.staticMin = c
         obj.resetMinMaxCounter += 1
- 
- 
-streams.serial()
- 
-# semaphore initialized to 0 -> red: if a thread tries to acquire the semaphore
-# it blocks if the semaphore has not been turned 'green' (released)
-semaphore = threading.Semaphore(0)
-toishield.microphone.detectionEnabled = True
-toishield.microphone.activePumpkin = False
-hsong = rtttl.tune('Halloween:d=4,o=5,b=180:8d6,8g,8g,8d6,8g,8g,8d6,8g,8d#6,8g,8d6,8g,8g,8d6,8g,8g,8d6,8g,8d#6,8g,8c#6,8f#,8f#,8c#6,8f#,8f#,8c#6,8f#,8d6,8f#,8c#6,8f#,8f#,8c#6,8f#,8f#,8c#6,8f#,8d6,8f#')
-toishield.microphone.staticMin = 4096
-toishield.microphone.staticMax = -4096
-toishield.microphone.resetMinMaxCounter = 0
- 
-leds = neo.ledstrip(toishield.led_pin, 16)
-leds.setall(0,0,0)
-leds.on()
+
+# set 'detectSound' as the function to be applied to the object at every sampling step    
 toishield.microphone.doEverySample(detectSound)
-# 20Hz-1kHz
+# start sampling at 1 millisecond (1 kHZ) with a window length of 50 so that
+# the lowest detectable frequency is 20 Hz
 toishield.microphone.startSampling(1,50,"raw")
